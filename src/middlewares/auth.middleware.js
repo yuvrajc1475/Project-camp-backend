becca8f088f424f6899e9d317df524e737715ddf
Yuvraj <yuvrajchopra9817239199@gmail.com>
2026-02-15 13:48:19 +0530

    add auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,31 @@
+import { User } from "../models/user.models.js"
+import { asyncHandler } from "./utils/async-handler.js"
+import { ApiError } from "../utils/api-error.js"
+import jwt from "jsonwebtoken"
+
+
+
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    const token=req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+    
+    if (!token) {
+        throw new ApiError(401,"Unauthorized request")
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET) 
+        const user = await User.findById(decodedToken?._id).select(
+            "-password -refreshToken -emailVerificationToken -emailVerificationExpiry");
+        
+        if (!user) {
+          throw new ApiError(401, "Inavlid access token");
+        }
+        req.user = user
+        next()
+    } catch (error) {
+        throw new ApiError(401, "Inavlid access token");
+
+        
+    }
+})
