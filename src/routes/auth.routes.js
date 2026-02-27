93a773b1f2990c4023f6d09aece9c1a0ac274d0b
Yuvraj <yuvrajchopra9817239199@gmail.com>
2026-02-27 19:14:27 +0530

    add routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { login, logoutUser, registerUser } from "../controllers/auth.controllers.js";
+import { changeCurrentPassword, forgotPasswordReset, getCurrentUser, login, logoutUser, refreshAccessToken, registerUser, resendEmailverification, resetForgotPassword, verifyEmail } from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import {
   userRegistrationValidator,
   userLoginValidator,
+  userForgotPasswordValidator,
+  userResetForgotPasswordValidator,
+  userChangeCurrentPasswordValidator,
 } from "../validators/index.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -11,10 +14,24 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(userRegistrationValidator(),validate,registerUser);
 
-router.route("/login").post(userLoginValidator(),validate,login);
-router.route("/logout").post(verifyJWT,logoutUser);
+//unsecure routes
+router.route("/register").post(userRegistrationValidator(), validate, registerUser);
+
+router.route("/login").post(userLoginValidator(), validate, login);
+
+router.route("/verify-email/:verificationToken").get(verifyEmail);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/forgot-password").post(userForgotPasswordValidator(), validate, forgotPasswordReset);
+router.route("/verify-email/:verificationToken").get(verifyEmail);
+router.route("/reset-password/:resetToken").post(userResetForgotPasswordValidator(), validate, resetForgotPassword);
+
+
+//secure routes (verify jwt)
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/current-user").post(verifyJWT, getCurrentUser)
+router.route("/change-password").post(verifyJWT, userChangeCurrentPasswordValidator(), validate, changeCurrentPassword);
+router.route("/resend-email-verification").post(verifyJWT,resendEmailverification)
 
 
 export default router;
--- src/validators/index.js
+++ src/validators/index.js
@@ -33,9 +33,40 @@ const userLoginValidator = () => {
     ]
 }
 
+const userChangeCurrentPasswordValidator = () => {
+    return [
+      body("oldPassword").notEmpty().withMessage("Old password is required"),
+      body("newPassword").notEmpty().withMessage("New password is required"),
+    ];
+}
+
+const userForgotPasswordValidator = () => {
+    return [
+        body("email")
+            .notEmpty()
+            .withMessage("Email is required")
+            .isEmail()
+            .withMessage("Email is invalid")
+    ]
+}
+
+const userResetForgotPasswordValidator = () => {
+    return [
+        body("newPassword")
+            .notEmpty()
+            .withMessage("Password is required")
+    ]
+}
 
 
 
 
 
-export { userRegistrationValidator, userLoginValidator };
+
+export {
+  userRegistrationValidator,
+  userLoginValidator,
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userResetForgotPasswordValidator,
+};
