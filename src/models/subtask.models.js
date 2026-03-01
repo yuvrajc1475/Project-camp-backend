0144324f575a51492dfe43c3bca3cb7a26984d25
Yuvraj <yuvrajchopra9817239199@gmail.com>
2026-03-01 13:37:20 +0530

    add schema for projects,tasks,notes

---
+++ src/models/subtask.models.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const subTaskSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim:true
+    },
+    task: {
+        type: Schema.Types.ObjectId,
+        ref: "Task",
+        required:true
+    },
+    isCompleted: {
+        type: Boolean,
+        default:false
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required:true
+    }
+})
+
+export const Subtask = mongoose.model("Subtask", subTaskSchema);
---
+++ src/models/task.models.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+import { AvailableTaskStatues,TaskStatusEnum } from "../utils/constants.js";
+
+
+const taskSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim:true
+    },
+    description: String,
+    project: {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        required:true
+    },
+    assignedTo: {
+        type: Schema.Types.ObjectId,
+        ref:"User"
+    },
+    assignedBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    status: {
+        type: String,
+        enum: AvailableTaskStatues,
+        default:TaskStatusEnum.TODO
+    },
+    attachments: {
+        type: [{
+            url: String,
+            MimeType: String,
+            size:Number
+        }],
+        default:[]
+    }
+}, { timestamps: true })
+
+export const Task= mongoose.model("Task",taskSchema)
