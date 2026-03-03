2cf6d916981b327c7982c875cd2154cde5fb429d
Yuvraj <yuvrajchopra9817239199@gmail.com>
2026-03-03 11:53:26 +0530

    add aggregation pipeline for getProjects

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -7,7 +7,47 @@ import { ProjectMember } from "../models/projectmember.models.js";
 import mongoose from "mongoose";
 import { UserRolesEnum } from "../utils/constants.js";
 const getProjects = asyncHandler(async (req, res) => {
-    //test
+    const projects = await ProjectMember.aggregate([
+    {
+        $match: {
+          user: new mongoose.Types.ObjectId(req.user._id),
+        },
+    },
+    {
+        $lookup: {
+            from: "projectmembers",
+            localField: "_id",
+            foreignField: "projects",
+           as:"projectmembers"
+        }
+        },
+        {
+            $addFields: {
+                members: {
+                $size:"projectmembers",
+            }
+        }
+        },
+        {
+            $unwind:"$project"
+        },
+        {
+            $project: {
+                project: {
+                    _id:1,
+                    name: 1,
+                    description: 1,
+                    members: 1,
+                    createdAt: 1,
+                    createdBy:1
+                },
+                role: 1,
+                _id:0
+            }
+        }
+    ]);
+
+    return res.status(200).json(new ApiResponse(200,projects,"Projects fetched successfully"))
 })
 
 const getProjectById = asyncHandler(async (req, res) => {
