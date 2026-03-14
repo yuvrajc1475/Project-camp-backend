0354a244804e86ccf3244915c0c4e3382dffe583
Yuvraj <yuvrajchopra9817239199@gmail.com>
2026-03-14 11:56:43 +0530

    add project-based RBAC middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,9 @@
 import { User } from "../models/user.models.js"
+import {ProjectMember} from "../models/projectmember.models.js"
 import { asyncHandler } from "../utils/async-handler.js"
 import { ApiError } from "../utils/api-error.js"
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
 
 
 
@@ -29,3 +31,39 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
         
     }
 })
+
+
+export const validateProjectPermission = (roles = []) => {
+    asyncHandler(async (req, res, next) => {
+        const { projectId } = req.params
+        
+        if (!projectId) {
+            throw new ApiError(400,"project id is missing")
+        }
+
+        const project=await ProjectMember.findOne({
+            project: new mongoose.Types.ObjectId(projectId),
+            user:new mongoose.Types.ObjectId(req.user._id)
+        })
+
+        if (!project) {
+          throw new ApiError(400, "project  is missing");
+        }
+
+        const givenRole = project?.role
+        
+        req.user.role = givenRole
+        
+        if (!roles.includes(givenRole)) {
+            throw new ApiError(
+                403,
+                "you donot have permission to perform this action"
+            )
+        }
+
+        next()
+
+
+
+    })
+}
