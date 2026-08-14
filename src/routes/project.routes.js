24c48d7e57230c34055ae013772ddbb3717b69c4
Yuvraj <yuvrajchopra9817239199@gmail.com>
2026-08-14 23:27:45 +0530

    Fix constants name to AvailableUserRole singular

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -19,7 +19,7 @@ import {
   verifyJWT,
   validateProjectPermission,
 } from "../middlewares/auth.middleware.js";
-import { AvailableUserRoles, UserRolesEnum } from "../utils/constants.js";
+import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
 
 const router = Router();
 router.use(verifyJWT);
@@ -31,7 +31,7 @@ router
 
 router
   .route("/:projectId")
-  .get(validateProjectPermission(AvailableUserRoles), getProjectById)
+  .get(validateProjectPermission(AvailableUserRole), getProjectById)
   .put(
     validateProjectPermission([UserRolesEnum.ADMIN]),
     createProjectValidator(),
