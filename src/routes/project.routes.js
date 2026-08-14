6dd496d327c5e8d5c790e282f9e6e7a6fb79786f
Yuvraj <yuvrajchopra9817239199@gmail.com>
2026-08-14 20:08:09 +0530

    Remove duplicate import statement

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -8,54 +8,51 @@ import {
   getProjectMembers,
   updateProject,
   deleteProject,
-  updateProject,
   updateMemberRole,
 } from "../controllers/project.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import {
-  createProjectValidator,addMemberToProjectValidator
+  createProjectValidator,
+  addMemberToProjectValidator,
 } from "../validators/index.js";
-import { verifyJWT,validateProjectPermission } from "../middlewares/auth.middleware.js";
+import {
+  verifyJWT,
+  validateProjectPermission,
+} from "../middlewares/auth.middleware.js";
 import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
 
-
-
 const router = Router();
-router.use(verifyJWT)
-
+router.use(verifyJWT);
 
 router
-    .route("/")
-    .get(getProjects)
-    .post(createProjectValidator(),validate,createProject)
+  .route("/")
+  .get(getProjects)
+  .post(createProjectValidator(), validate, createProject);
 
 router
   .route("/:projectId")
-  .get(validateProjectPermission(AvailableUserRole),getProjectById)
-    .put(
-        validateProjectPermission([UserRolesEnum.ADMIN]),
-        createProjectValidator(),
-        validate,
-        updateProject
-    )
-    .delete(
-        validateProjectPermission([UserRolesEnum.ADMIN]),
-        deleteProject
-    );
+  .get(validateProjectPermission(AvailableUserRole), getProjectById)
+  .put(
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    createProjectValidator(),
+    validate,
+    updateProject,
+  )
+  .delete(validateProjectPermission([UserRolesEnum.ADMIN]), deleteProject);
 
 router
-    .route("/:projectId/members")
-    .get(getProjectMembers)
-    .post(
-        validateProjectPermission([UserRolesEnum.ADMIN]),
-        addMemberToProjectValidator(),
-        validate,
-        addMembersToProject
-)
-    
+  .route("/:projectId/members")
+  .get(getProjectMembers)
+  .post(
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    addMemberToProjectValidator(),
+    validate,
+    addMembersToProject,
+  );
+
 router
   .route("/:projectId/members/:userId")
   .put(validateProjectPermission([UserRolesEnum.ADMIN]), updateMemberRole)
-  .delete(validateProjectPermission([UserRolesEnum.ADMIN]),deleteMember);
+  .delete(validateProjectPermission([UserRolesEnum.ADMIN]), deleteMember);
 
-export default router
+export default router;
